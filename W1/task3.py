29912d304db48f01c5f32e21fcbd0c91a175e41e
polalbacar <pol.albacar@hotmail.com>
2021-03-07 17:12:19 +0100

    added task3.py and data folder

---
+++ W1/task3.py
@@ -0,0 +1,11 @@
+def task3_1(): 
+    print("Task 3 - Quantitative evaluation of optical flow")
+
+    ground_truth_path = "data/of_ground_truth"
+    test_path = "data/of_estimations_LK"
+
+    
+
+
+if __name__ == "__main__":
+    task3_1()
