12ef458f98636ca7b585b1aaf9d2fbd2712d9db7
EddieMg <eduardmgobierno@gmail.com>
2021-03-07 17:16:03 +0100

    Added func to read flow

--- W1/task3.py
+++ W1/task3.py
@@ -1,3 +1,8 @@
+import cv2
+
+
+
+
 def task3_1(): 
     print("Task 3 - Quantitative evaluation of optical flow")
 
@@ -5,6 +10,26 @@ def task3_1():
     test_path = "data/of_estimations_LK"
 
     
+
+def read_flow (file)
+
+    im = cv2.imread(file, cv2.IMREAD_UNCHANGED).astype(np.double)
+
+    # (u,v) flow vector
+    im_u = (im[:, :, 2] - 2 ** 15) / 64
+    im_v = (im[:, :, 1] - 2 ** 15) / 64
+
+    # pixel exists or not
+    im_valid = im[:, :, 0]
+    im_valid[im_valid > 1] = 1
+
+    im_u[im_valid == 0] = 0
+    im_v[im_valid == 0] = 0
+
+    flow_field = np.dstack((im_u, im_v, im_valid))
+
+    return flow_field
+
 
 
 if __name__ == "__main__":
