75dbbd2db92968e6a95108c8518c1dddefeb4caf
EddieMg <eduardmgobierno@gmail.com>
2021-03-07 19:39:11 +0100

    Completed task 3

--- W1/task3.py
+++ W1/task3.py
@@ -1,9 +1,11 @@
 import cv2
 from os import path
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def read_flow (of_file):
-    im = cv2.imread(of_file, cv2.IMREAD_UNCHANGED).astype(np.double)
+    im = cv2.imread(of_file, cv2.IMREAD_UNCHANGED).astype(np.double) #the decoded images will have the channels stored in B G R order.
 
     im_u = (im[:, :, 2] - 2 ** 15) / 64
     im_v = (im[:, :, 1] - 2 ** 15) / 64
@@ -21,17 +23,26 @@ def read_flow (of_file):
 def compute_msen (ground_truth, predicted):
     u_diff = ground_truth[:,:,0] - predicted[:,:,0]
     v_diff = ground_truth[:,:,1] - predicted[:,:,1]
-
+    
     se = np.sqrt(u_diff ** 2 + v_diff ** 2)
     sen = se[ground_truth[:,:,2]==1]
+
+    se[ground_truth[:,:,2]==0] = 0 #To exclude the non valid
+    img_plot = plt.imshow(se)
+    img_plot.set_cmap("nipy_spectral")
+    plt.colorbar()
+    plt.show()
+
+    # img_plot = plt.imshow(predicted)
+    # img_plot.set_cmap("nipy_spectral")
+    # plt.colorbar()
+    # plt.show()
 
     msen = np.mean(sen)
 
     return msen,sen
 
-def compute_pepn(ground_truth,predicted,th=3):
-
-    _,sen = compute_msen(ground_truth,predicted)
+def compute_pepn(ground_truth,predicted,sen,th=3):
 
     n_pixels_n= len(sen)
 
@@ -42,28 +53,37 @@ def compute_pepn(ground_truth,predicted,th=3):
     return pepn
 
 
-def task3_1(): 
+def task3_1_2(ground_truth_path,estimated_path,frame): 
     print("Task 3 - Quantitative evaluation of optical flow")
+
+    ground_truth = read_flow(path.join(ground_truth_path, frame))
+    estimated_flow = read_flow(path.join(estimated_path, "LKflow_" + frame))
+
+    msen,sen = compute_msen(ground_truth,estimated_flow)
+
+    pepn = compute_pepn(ground_truth,estimated_flow,sen)
+    print(msen,pepn) # put the outputs nicer
+
+    return msen,pepn,sen
+
+
+
+def task3_3 (sen,frame):
+    print("Task 3.3 - Visualization of Optical flow error")
+
+    plt.hist(x=sen,bins=50)
+    plt.savefig(frame)
+    plt.clf()
+
+if __name__ == "__main__":
+
 
     ground_truth_path = "../data/of_ground_truth"
     estimated_path = "../data/of_estimations_LK"
     frames = ["000045_10.png","000157_10.png"]
 
+
     for frame in frames:
-        gt_flow = read_flow(path.join(ground_truth_path, frame))
-        estimated_flow = read_flow(path.join(estimated_path, "LKflow_" + frame))
-    
-        msen,sen = compute_msen(gt_flow,estimated_flow)
+        msen,pepn,sen = task3_1_2(ground_truth_path,estimated_path,frame)
 
-        pepn = compute_pepn(gt_flow,estimated_flow)
-        print(msen,pepn) # put the outputs nicer
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    task3_1()
+        asfd = task3_3(sen,frame)
