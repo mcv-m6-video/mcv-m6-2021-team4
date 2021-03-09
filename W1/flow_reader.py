4bcbee33e83ca7f8b332043a572dc8c448284dcd
polalbacar <pol.albacar@hotmail.com>
2021-03-09 18:21:15 +0100

    comments documentation

--- W1/flow_reader.py
+++ W1/flow_reader.py
@@ -6,7 +6,12 @@ def read_flow(flow_file):
     # channels: BGR
     im = cv2.imread(flow_file, cv2.IMREAD_UNCHANGED).astype(np.double)
 
-    # comentar una mica pq es fa això i què és im_exists
+    # Optical flow maps are saved as 3-channel uint16 PNG images: The first channel
+    # contains the u-component, the second channel the v-component and the third
+    # channel denotes if a valid ground truth optical flow value exists for that
+    # pixel (1 if true, 0 otherwise). To convert the u-/v-flow into floating point
+    # values, convert the value to float, subtract 2^15 and divide the result by 64.
+
     im_u = (im[:, :, 2] - 2 ** 15) / 64
     im_v = (im[:, :, 1] - 2 ** 15) / 64
 
