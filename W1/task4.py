7e07d40a39aace53d615b3e9f7f3673ed30b12db
polalbacar <pol.albacar@hotmail.com>
2021-03-09 22:39:57 +0100

    task4 option2 for both frames

--- W1/task4.py
+++ W1/task4.py
@@ -42,47 +42,50 @@ def option2():
     gt_path = "../data/of_ground_truth"
     frames = ["000045_10.png", "000157_10.png"]
 
-    gt = read_flow(path.join(gt_path, frames[0]))
-    X, Y = np.meshgrid(np.arange(0, gt.shape[1], 1), np.arange(0, gt.shape[0], 1))
+    for frame in frames:
 
-    originalImage = cv2.imread(path.join(data_path, frames[0]))
-    grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+        gt = read_flow(path.join(gt_path, frame))
+        X, Y = np.meshgrid(np.arange(0, gt.shape[1], 1), np.arange(0, gt.shape[0], 1))
+
+        originalImage = cv2.imread(path.join(data_path, frame))
+        grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
 
-    U = gt[:, :, 0]
-    V = gt[:, :, 1]
+        U = gt[:, :, 0]
+        V = gt[:, :, 1]
 
-    print(originalImage.shape)
-    windowSize = 50
+        print(originalImage.shape)
+        windowSize = 50
 
-    u_new = np.zeros([originalImage.shape[0], originalImage.shape[1]])
-    v_new = np.zeros([originalImage.shape[0], originalImage.shape[1]])
+        u_new = np.zeros([originalImage.shape[0], originalImage.shape[1]])
+        v_new = np.zeros([originalImage.shape[0], originalImage.shape[1]])
 
-    print(u_new.shape)
-    print(v_new.shape)
+        print(u_new.shape)
+        print(v_new.shape)
 
-    for i in range(0, originalImage.shape[0]-windowSize, windowSize):
-        for j in range(0, originalImage.shape[1]-windowSize, windowSize):
-            print(i, j)
+        for i in range(0, originalImage.shape[0]-windowSize, windowSize):
+            for j in range(0, originalImage.shape[1]-windowSize, windowSize):
+                print(i, j)
 
-            u_mean = np.mean(U[i:i+windowSize, j:j+windowSize])
-            v_mean = np.mean(V[i:i+windowSize, j:j+windowSize])
-            
-            u_new[i+int(windowSize/2), int(j+windowSize/2)] = u_mean
-            v_new[i+int(windowSize/2), int(j+windowSize/2)] = v_mean
-            print("MEAN: ", u_mean, v_mean)
+                u_mean = np.mean(U[i:i+windowSize, j:j+windowSize])
+                v_mean = np.mean(V[i:i+windowSize, j:j+windowSize])
+                
+                u_new[i+int(windowSize/2), int(j+windowSize/2)] = u_mean
+                v_new[i+int(windowSize/2), int(j+windowSize/2)] = v_mean
+                print("MEAN: ", u_mean, v_mean)
 
-    plt.figure()
-    plt.title("pivot='tip'; scales with x view")
-    M = np.hypot(u_new, v_new)
-    Q = plt.quiver(X[::5, ::5], Y[::5, ::5], u_new[::5, ::5], v_new[::5, ::5], M[::5, ::5],
-                    units='x', pivot='tail', width=3, scale=0.1)
-    qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$',
-                        labelpos='E', coordinates='data')
+        plt.figure()
+        plt.title("pivot='tip'; scales with x view")
+        M = np.hypot(u_new, v_new)
+        Q = plt.quiver(X[::5, ::5], Y[::5, ::5], u_new[::5, ::5], v_new[::5, ::5], M[::5, ::5],
+                        units='x', pivot='tail', width=3, scale=0.025)
+        qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$',
+                            labelpos='E', coordinates='data')
 
-    plt.imshow(grayImage, cmap='gray')
-
-    plt.show()
+        plt.imshow(grayImage, cmap='gray')
+        # plt.savefig('task4_opt2_'+frame)    
+        plt.show()
+    
 
 
 
