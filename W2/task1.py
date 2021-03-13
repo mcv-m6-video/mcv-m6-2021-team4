141e245cb96d19bcdefbbe6d594d0e5a5d7692cd
polalbacar <pol.albacar@hotmail.com>
2021-03-13 17:55:07 +0100

    mean computed reading video avi

--- W2/task1.py
+++ W2/task1.py
@@ -6,30 +6,58 @@ import matplotlib.pyplot as plt
 
 ##Very naive approach, need to use a lib to compute on the fly with the video.
 
-path_to_frames_folder = r"C:\Users\Eduard\Desktop\M6\AICity_data\train\S03\c010\img\*.bmp"
+# path_to_frames_folder = r"C:\Users\Eduard\Desktop\M6\AICity_data\train\S03\c010\img\*.bmp"
+path_video = "data/vdo.avi"
+vidcap = cv2.VideoCapture(path_video)
+frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+print(frame_count)
 
-list_frames = [frame for frame in glob.glob(path_to_frames_folder)]
-print(len(list_frames))
+train_len = int(0.25*frame_count)
+test_len = frame_count-train_len
+
+print("train_len: ",train_len)
+print("test_len: ",test_len)
+
 img_list=[]
 
-for image in list_frames[:25]:
-    img_list.append(cv2.imread(image)) 
-    # img_list.append(cv2.imread(image,cv2.COLOR_BGR2RGB)) 
+success,image = vidcap.read()
+count = 0
+while count < 25:
+  success,image = vidcap.read()
+  img_list.append(image) 
+  count += 1
+
+#Compute mean 
+mean_all_frames= np.mean(img_list,axis=0)
+print("mean_all_frames.shape", mean_all_frames.shape)
+mean_all_frames = mean_all_frames.astype(int)
+mean_all_frames = cv2.convertScaleAbs(mean_all_frames)
+cv2.imwrite("d2.bmp",mean_all_frames)
+
+
+
+# list_frames = [frame for frame in glob.glob(path_to_frames_folder)]
+# print(len(list_frames))
+# img_list=[]
+
+# for image in list_frames[:25]:
+#     img_list.append(cv2.imread(image)) 
+#     # img_list.append(cv2.imread(image,cv2.COLOR_BGR2RGB)) 
 
     
 
-print(len(img_list))
-print(img_list[0][0])
+# print(len(img_list))
+# print(img_list[0][0])
 
-mean_all_frames= np.mean(img_list,axis=0)
-print(mean_all_frames.shape)
-print(mean_all_frames[0])
+# mean_all_frames= np.mean(img_list,axis=0)
+# print(mean_all_frames.shape)
+# print(mean_all_frames[0])
 
-print(type(mean_all_frames))
-mean_all_frames = mean_all_frames.astype(int)
-mean_all_frames = cv2.convertScaleAbs(mean_all_frames)
-cv2.imwrite("d.bmp",mean_all_frames)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# plt.imshow(mean_all_frames.astype(int),cmap='gray')
-# plt.show()
+# print(type(mean_all_frames))
+# mean_all_frames = mean_all_frames.astype(int)
+# mean_all_frames = cv2.convertScaleAbs(mean_all_frames)
+# cv2.imwrite("d.bmp",mean_all_frames)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# # plt.imshow(mean_all_frames.astype(int),cmap='gray')
+# # plt.show()
