b9d396b1757f92636987478ec420de4d988e7728
polalbacar <pol.albacar@hotmail.com>
2021-03-13 19:29:47 +0100

    swap variance to std, saving results

--- W2/task1.py
+++ W2/task1.py
@@ -21,9 +21,11 @@ print("test_len: ",test_len)
 img_list=[]
 
 success,frame = vidcap.read()
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 count = 0
 while count < train_len:
   success,frame = vidcap.read()
+  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   img_list.append(frame) 
   count += 1
 
@@ -31,9 +33,10 @@ while count < train_len:
 mean_train_frames= np.mean(img_list,axis=0)
 print("mean_train_frames.shape", mean_train_frames.shape)
 
-#Compute variance 
-variance_train_frames= np.var(img_list,axis=0)
-print("variance_train_frames.shape", variance_train_frames.shape)
+#Compute std 
+std_train_frames= np.std(img_list,axis=0)
+cv2.imwrite("std.png", std_train_frames)
+print("std_train_frames.shape", std_train_frames.shape)
 
 # vidcap.set(2,train_len)
 # ret, frame = vidcap.read()
@@ -43,39 +46,16 @@ print("variance_train_frames.shape", variance_train_frames.shape)
 #     print(count)
 
 
-alfa =2
+alfa = 3
 img_list_processed=[]
+segmentation = np.zeros((1080,1920))
+while count < train_len + 5:
+    success,frame = vidcap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    segmentation[abs(frame - mean_train_frames)>= alfa*(std_train_frames + 2)] = 255
 
-while count < train_len + 2:
-  success,image = vidcap.read()
-  image[abs(image - mean_train_frames)>= alfa*(variance_train_frames + 2)] = 0
-  img_list_processed.append(image) 
-  count += 1
+    img_list_processed.append(segmentation) 
+    count += 1
 
-cv2.imwrite("d.bmp",img_list_processed[0].astype(int))
-
-# list_frames = [frame for frame in glob.glob(path_to_frames_folder)]
-# print(len(list_frames))
-# img_list=[]
-
-# for image in list_frames[:25]:
-#     img_list.append(cv2.imread(image)) 
-#     # img_list.append(cv2.imread(image,cv2.COLOR_BGR2RGB)) 
-
-    
-
-# print(len(img_list))
-# print(img_list[0][0])
-
-# mean_all_frames= np.mean(img_list,axis=0)
-# print(mean_all_frames.shape)
-# print(mean_all_frames[0])
-
-# print(type(mean_all_frames))
-# mean_all_frames = mean_all_frames.astype(int)
-# mean_all_frames = cv2.convertScaleAbs(mean_all_frames)
-# cv2.imwrite("d.bmp",mean_all_frames)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-# # plt.imshow(mean_all_frames.astype(int),cmap='gray')
-# # plt.show()
+for i,img in enumerate(img_list_processed):
+    cv2.imwrite("seg_" + str(alfa) + "_" + str(i) + ".bmp", img.astype(int))
