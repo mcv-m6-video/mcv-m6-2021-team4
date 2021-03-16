9be59e0aec95cbfd058236aa4035c5bbb1da8f5b
Eduard Mainou <32465773+EddieMG@users.noreply.github.com>
2021-03-16 13:46:55 +0100

    F*k identation

--- W2/bg_estimation.py
+++ W2/bg_estimation.py
@@ -72,7 +72,7 @@ def adaptive_bg_est(image,frame_size, mean, std, params):
     h,w = frame_size
     mask = abs(image - mean) >= alpha * (std + 2)
     roi = cv2.imread(params['roi_path'],cv2.IMREAD_GRAYSCALE)
-     _,roi = cv2.threshold(roi,127,255,cv2.THRESH_BINARY)
+    _,roi = cv2.threshold(roi,127,255,cv2.THRESH_BINARY)
     segmentation = np.zeros((h, w))
     nc = color_space[params['color_space']][1]
     
