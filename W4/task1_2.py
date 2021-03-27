c8137a1d9574e6212d643204dd3774a8bde6c625
EddieMg <eduardmgobierno@gmail.com>
2021-03-27 13:50:58 +0100

    Pyflow working

--- W4/task1_2.py
+++ W4/task1_2.py
@@ -1,10 +1,14 @@
-import pyflow
-
+import sys
+from PIL import Image
+import time
+import numpy as np
 sys.path.append("W1")
-from flow_evaluation.py import compute_pepn,compute_msen
+sys.path.append("W4/pyflow")
+import pyflow
+from flow_evaluation import compute_pepn,compute_msen
 
-im1 = np.array(Image.open('examples/car1.jpg'))
-im2 = np.array(Image.open('examples/car2.jpg'))
+im1 = np.array(Image.open('data/000045_10.png'))
+im2 = np.array(Image.open('data/000045_11.png'))
 im1 = im1.astype(float) / 255.
 im2 = im2.astype(float) / 255.
 
@@ -19,3 +23,24 @@ colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
 
 
 
+
+s = time.time()
+u, v, im2W = pyflow.coarse2fine_flow(
+    im1, im2, alpha, ratio, minWidth, nOuterFPIterations, nInnerFPIterations,
+    nSORIterations, colType)
+e = time.time()
+print('Time Taken: %.2f seconds for image of size (%d, %d, %d)' % (
+    e - s, im1.shape[0], im1.shape[1], im1.shape[2]))
+flow = np.concatenate((u[..., None], v[..., None]), axis=2)
+np.save('W4/outFlow.npy', flow)
+
+if True:
+    import cv2
+    hsv = np.zeros(im1.shape, dtype=np.uint8)
+    hsv[:, :, 0] = 255
+    hsv[:, :, 1] = 255
+    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv[..., 0] = ang * 180 / np.pi / 2
+    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    cv2.imwrite('W4/outFlow_new.png', rgb)
